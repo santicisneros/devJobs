dbcff52f1555ba965f48fa745faa1e7b2a0e8581
scisneros <santi.cisneros@live.com.ar>
2023-03-20 23:49:00 -0300

    se conecta base de datos, se da contenido a pagprincial y se investiga sobre connect-mongodb-session para almacenar sessiones

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-
+const mongoose = require('mongoose')
+require('./config/db');
 const express = require('express');
 const router = require('./routes/index');
 const exphbs = require('express-handlebars')
 const path = require ('path')
+const cookieParser =require('cookie-parser');
+const session = require('express-session')
 const app =express();
+const MongoStore =require('connect-mongodb-session')(session);/*SE LE PASA VARIABLES EN SESSION AL PAQUETE CONNECT*/
+const store = new MongoStore({
+    uri: process.env.DATABASE,
+    collection:'devJobs'
+}); 
+require('dotenv').config({path:'variables.env'})
 
 app.engine('handlebars',
      exphbs.engine({
@@ -11,12 +20,27 @@ app.engine('handlebars',
 }));
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use 
+
+app.use(cookieParser());
+/**SE INTENTA APLICAR SESSION CON MONGO */
+
+app.use(
+    session({
+        secret: process.env.SECRETO,
+        key: process.env.KEY,
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+         
+    })
+);
 
 
 
 app.use('/',router())
 
-
-app.listen(1000,(req,res)=>{
-    console.log('puerto corriendo en 10000')
+PORT = process.env.PUERTO
+app.listen(PORT,(req,res)=>{
+    console.log(`server run in ${PORT}`)
 })
